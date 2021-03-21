8f9ac2c5ad2b9006bfe86c845d38ff7768ca284e
ahmet2021 <yilmazsrahmet@gmail.com>
2021-03-21 13:51:48 +0300

    indroduction package is completed

--- src/introduction/Scanner01AndPrint.java
+++ src/introduction/Scanner01AndPrint.java
@@ -1,5 +1,7 @@
 package introduction;
 
+import java.util.Scanner;
+
 public class Scanner01AndPrint {
 
 	public static void main(String[] args) {
@@ -10,6 +12,21 @@ public class Scanner01AndPrint {
 		//"print" command will print and wait in the same line
 		System.out.print("first line use 'print' command");
 		System.out.println(" / second line use 'println' command");
+		
+		//To communicate with the user we use Scanner Class
+		
+		//STEP 1
+		Scanner scan = new Scanner(System.in);
+		
+		//STEP 2
+		System.out.println("Enter your age");
+		
+		//STEP 3
+		int age = scan.nextInt();
+		System.out.println(age);
+		
+		scan.close();
+		
 	}
 
 }
---
+++ src/introduction/Scanner04.java
@@ -0,0 +1,27 @@
+package introduction;
+
+import java.util.Scanner;
+
+public class Scanner04 {
+
+	public static void main(String[] args) {
+
+		//Ask user to enter full name and address and print them on the console
+		
+	//	Class Name   Object Name   Equal Sign    new Keyword      Constructor
+		Scanner      scan          =             new              Scanner(System.in);
+		System.out.println("Enter you first and last name");
+		String fullName = scan.nextLine(); //For Strings we nextLine() method
+		                                   //There is one more method for Strings which is next()
+		                                   //If you use next(), you can get just the first word in the String		
+		System.out.println("Enter your address");
+		String address = scan.nextLine();
+		
+		System.out.println(fullName);
+		System.out.println(address);
+		
+		scan.close();
+		
+	}
+
+}
